e3099138d8ee3423444068bad0edc4893bdcaaae
Ico-code <nico.nikusti@gmail.com>
2024-12-06 12:17:32 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -170,7 +170,6 @@ const App: React.FC = () => {
                     path="/tasks/:listId"
                     render={() => (
                       <TaskList
-                        key={activeTaskList.length}
                         currentTaskList={activeTaskList}
                         updateCurrentTaskList={updateCurrentTaskList}
                       />
